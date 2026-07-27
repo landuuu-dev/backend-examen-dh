3df2dfafad572e0353f6fe0236f1a4ace2221afd
landuuu-dev <olachao32@gmail.com>
2026-07-27 13:54:27 -0400

    commit anterior malo, este es el bueno

--- src/main/java/dh/tour/controllers/AdminController.java
+++ src/main/java/dh/tour/controllers/AdminController.java
@@ -1,32 +1,4 @@
 package dh.tour.controllers;
-
-import dh.tour.service.UsuarioService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/admin")
-public class AdminController {
-
-    private final UsuarioService usuarioService;
-
-    public AdminController(UsuarioService usuarioService) {
-        this.usuarioService = usuarioService;
-    }
-
-    @PostMapping("/promote/{id}")
-    @PreAuthorize("hasAnyAuthority('SUPER_ADMIN', 'ROLE_SUPER_ADMIN')")
-    public ResponseEntity<?> makeAdmin(@PathVariable String id) {
-        usuarioService.promoteToAdmin(id);
-        return ResponseEntity.ok("Usuario promovido a ADMIN");
-    }
-
-    package dh.tour.controllers;
-
 import dh.tour.service.UsuarioService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -60,4 +32,3 @@ import org.springframework.web.bind.annotation.RestController;
             return ResponseEntity.ok("Usuario degradado a USER correctamente");
         }
     }
-}
